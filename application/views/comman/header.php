3645e3913d63b4bf4b85840cb5442da5d7d7fe32
raviribadiya18 <raviribadiya014@gmail.com>
2020-02-17 11:07:07 +0530

    heade and footer add menu

--- application/views/comman/header.php
+++ application/views/comman/header.php
@@ -1,93 +1,88 @@
 
 <body class="counter-scroll">
-	<div id="loading-overlay">
-		<div class="loader"></div>
-	</div>
-	<header id="header" class="header bg-color">
-		<div class="container">
-			<div class="flex-header">
-				<div id="logo" class="logo">
-					<a href="<?=base_url();?>" title="Logo"><img src="<?=base_url();?>assets/images/logo/redrawn-logo.png" data-width="80" data-height="34" alt="images" data-retina="<?=base_url();?>assets/images/logo/redrawn-logo.png"></a>
-				</div>
-				<div class="content-menu">
-					<div class="nav-wrap">
-						<div class="btn-menu"><span></span></div>
-						<nav id="mainnav" class="mainnav">
-							<ul class="menu">
-								<!-- <li class="<?php echo activate_menu('home'); ?>"><a href="<?php echo site_url();?>">Home</a></li>
-								<li class="<?php echo activate_menu('about'); ?>"><a href="<?php echo site_url('about');?>">About us</a></li>
-								<li>
-									<a href="#">Our Services</a>
-									<ul class="sub-menu">
-										tli class="<?php echo activate_menu('Testimonials'); ?>t><a href="<?php echo site_url('Testimonials');?>">Testimonials</a></li>
-										<li class="<?php echo activate_menu('csr'); ?>"><a href="<?php echo site_url('csr');?>">CSR</a></li>
-                                    </ul>
-								</li>
-								<li class="<?php echo activate_menu('reverse_auction'); ?>"><a href="<?php echo site_url('reverse_auction');?>">Reverse Auction</a></li>
-								<li class="<?php echo activate_menu('contact'); ?>"><a href="<?php echo site_url('contact');?>">Contact Us</a></li>
- -->
-								<?php $uriMethod_name=$this->uri->segment(1);
-                                ?>
-                                <li <?php if($uriMethod_name==""){ echo "class='active'"; }else{ echo "class=''" ; } 
-                                    ?> ><a href="<?php echo base_url(); ?>">Home</a>
-                                </li>
-                                <li <?php if($uriMethod_name=="about"){ echo "class='active'"; }else{ echo "class=''" ; } 
-                                    ?> ><a href="<?php echo base_url(); ?>index.php/about">About Us</a>
-                                </li>
-                               	<li>
-									<a href="#">Our Services</a>
-									<ul class="sub-menu">
-                                        <li <?php if($uriMethod_name=="testimonials"){ echo "class='active'"; }else{ echo "class=''" ; } ?>><a href="<?php echo base_url(); ?>index.php/testimonials">Testimonials</a>
-                                		</li>
-                                		<li <?php if($uriMethod_name=="csr"){ echo "class='active'"; }else{ echo "class=''" ; } ?>><a href="<?php echo base_url(); ?>index.php/csr">CSR</a>
-                                		</li>
-                                    </ul>
-								</li>
-								 <li <?php if($uriMethod_name=="contact"){ echo "class='active'"; }else{ echo "class=''" ; } 
-                                    ?>><a href="<?php echo base_url(); ?>index.php/reverse_auction">Reverse Auction</a>
-                                </li>
-                                <li <?php if($uriMethod_name=="video"){ echo "class='active'"; }else{ echo "class=''" ; } 
-                                    ?>><a href="<?php echo base_url(); ?>index.php/video">Video</a>
-                                </li>
-                                <li <?php if($uriMethod_name=="contact"){ echo "class='active'"; }else{ echo "class=''" ; } 
-                                    ?>><a href="<?php echo base_url(); ?>index.php/contact">Contact Us</a>
-                                </li>
-                               	<!--<li>
-									<a href="<?=base_url();?>" class="active">Home</a>
-								</li>
-								<li>
-									<a href="<?=base_url();?>index.php/about">About Us</a>
-								</li>
-								<li>
-									<a href="#">Our Services</a>
-									<ul class="sub-menu">
-                                        <li><a href="<?=base_url();?>index.php/testimonials">Testimonials</a></li>
-                                        <li><a href="<?=base_url();?>index.php/csr">CSR</a></li>
-                                    </ul>
-								</li>
-								<li>
-									<a href="<?=base_url();?>index.php/Reverse_auction">Reverse Auction</a>
-								</li>
-								<li>
-									<a href="<?=base_url();?>index.php/contact">Contact Us</a>
-								</li>  -->
+<div id="loading-overlay">
+	<div class="loader"></div>
+</div>
+<header id="header" class="header bg-color">
+	<div class="container">
+		<div class="flex-header">
+			<div id="logo" class="logo">
+				<a href="<?=base_url();?>" title="Logo"><img src="<?=base_url();?>assets/images/logo/redrawn-logo.png" data-width="80" data-height="34" alt="images" data-retina="<?=base_url();?>assets/images/logo/redrawn-logo.png"></a>
+			</div>
+			<div class="content-menu">
+				<div class="nav-wrap">
+					<div class="btn-menu"><span></span></div>
+					<nav id="mainnav" class="mainnav">
+						<ul class="menu">
+							<!-- <li class="<?php echo activate_menu('home'); ?>"><a href="<?php echo site_url();?>">Home</a></li>
+							<li class="<?php echo activate_menu('about'); ?>"><a href="<?php echo site_url('about');?>">About us</a></li>
+							<li>
+								<a href="#">Our Services</a>
+								<ul class="sub-menu">
+									tli class="<?php echo activate_menu('Testimonials'); ?>t><a href="<?php echo site_url('Testimonials');?>">Testimonials</a></li>
+									<li class="<?php echo activate_menu('csr'); ?>"><a href="<?php echo site_url('csr');?>">CSR</a></li>
+                                </ul>
+							</li>
+							<li class="<?php echo activate_menu('reverse_auction'); ?>"><a href="<?php echo site_url('reverse_auction');?>">Reverse Auction</a></li>
+							<li class="<?php echo activate_menu('contact'); ?>"><a href="<?php echo site_url('contact');?>">Contact Us</a></li>
+-->
+							<?php $uriMethod_name=$this->uri->segment(1);
+                            ?>
+                            <li <?php if($uriMethod_name==""){ echo "class='active'"; }else{ echo "class=''" ; } 
+                                ?> ><a href="<?php echo base_url(); ?>">Home</a>
+                            </li>
+                            <li <?php if($uriMethod_name=="about"){ echo "class='active'"; }else{ echo "class=''" ; } 
+                                ?> ><a href="<?php echo base_url(); ?>index.php/about">About Us</a>
+                            </li>
+                              <li <?php if($uriMethod_name=="testimonials"){ echo "class='active'"; }else{ echo "class=''" ; } 
+                                ?> ><a href="<?php echo base_url(); ?>index.php/testimonials">Testimonials</a>
+                            </li>
+                            <li <?php if($uriMethod_name=="csr"){ echo "class='active'"; }else{ echo "class=''" ; } ?>><a href="<?php echo base_url(); ?>index.php/csr">CSR</a>
+                            </li>
+                            <li <?php if($uriMethod_name=="contact"){ echo "class='active'"; }else{ echo "class=''" ; }?>><a href="<?php echo base_url(); ?>index.php/reverse_auction">Reverse Auction</a>
+                            </li>
+                            <li <?php if($uriMethod_name=="video"){ echo "class='active'"; }else{ echo "class=''" ; } 
+                                ?>><a href="<?php echo base_url(); ?>index.php/video">Video</a>
+                            </li>
+                            <li <?php if($uriMethod_name=="contact"){ echo "class='active'"; }else{ echo "class=''" ; } 
+                                ?>><a href="<?php echo base_url(); ?>index.php/contact">Contact Us</a>
+                            </li>
+                           	<!--<li>
+								<a href="<?=base_url();?>" class="active">Home</a>
+							</li>
+							<li>
+								<a href="<?=base_url();?>index.php/about">About Us</a>
+							</li>
+							<li>
+								<a href="#">Our Services</a>
+								<ul class="sub-menu">
+                                    <li><a href="<?=base_url();?>index.php/testimonials">Testimonials</a></li>
+                                    <li><a href="<?=base_url();?>index.php/csr">CSR</a></li>
+                                </ul>
+							</li>
+							<li>
+								<a href="<?=base_url();?>index.php/Reverse_auction">Reverse Auction</a>
+							</li>
+							<li>
+								<a href="<?=base_url();?>index.php/contact">Contact Us</a>
+							</li>  -->
 
-							</ul>                                
-						</nav>
-					</div>
+						</ul>                                
+					</nav>
 				</div>
 			</div>
 		</div>
-	</header><!-- header -->
+	</div>
+</header><!-- header -->
 <div class="marq">
-	<marquee behavior="alternate" onmouseover="this.stop();" onmouseout="this.start();"> Helping Charities,Temples,Churches,Mosques,VWOs,NGOs etc to maximize your fund raising efforts.......Call: 91689889</marquee>
+<marquee behavior="alternate" onmouseover="this.stop();" onmouseout="this.start();"> Helping Charities,Temples,Churches,Mosques,VWOs,NGOs etc to maximize your fund raising efforts.......Call: 91689889</marquee>
 </div>
 <style type="text/css">
-	.marq{
-		background-color: black;
-		color: white;
-		font-family: arial;
-		height: 30px;
-		width: 100%;
-	}
+.marq{
+	background-color: black;
+	color: white;
+	font-family: arial;
+	height: 30px;
+	width: 100%;
+}
 </style>
--- application/views/testimonials_view.php
+++ application/views/testimonials_view.php
@@ -50,34 +50,6 @@
 			            </div>
 			        </div>
     			</div>
-                <div class="col-lg-4 col-md-12">
-                    <div class="featured-post">
-                        <div class="entry-image">
-                            <img src="<?=base_url();?>assets/images/practice/10.png" alt="images">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-8 col-md-12">
-                    <div class="flat-spacer" data-desktop="38" data-sdesktop="0" data-mobi="0" data-smobi="0"></div>
-                    <div class="content">
-                        <!-- <div class="caption">Our Features</div> -->
-                        <h4 class="heading">Testimonials</h4>
-                        <p class="description">
-                            "Craig is a fun and awesome emcee to have for your wedding!!"<br>
-                        	Joseph & Jessica
-                        </p>
-                        <p>
-							"Hi Craig,
-							It is my great pleasure to write this letter of recommendation for you.I have had the pleasure of employing you for my Wedding dinner, and I can state with confidence that you are a motivated and responsible person with a high degree of integrity. As such, I would strongly recommend your service to others. Craig is a humorous and motivated emcee who has a good feel and is incredibly knowledgeable in this field. As an emcee, he had a considerable amount of responsibilities, and he handled them effortlessly. He showed his skills to communicate with guest and VIPS, and due to his dedication and attention to detail, our guests thoroughly enjoyed themselves on the wedding night. Craig takes the initiative to identify problems and devise solutions. His role requires strong communication skills, and he has shown that he not only has these skills, but that he also has the ability to provide a calming presence for us on the night itself when most newly weds would experience a high level of stress. His experience in events management is almost unparalleled all of which is highly reflected by the very good advice that was rendered. I would therefore highly recommend you to anyone who are looking for a master of ceremony without any reservation. Lastly, thanks for your help Craig and also for a very successful wedding."
-                        </p>
-                        <p>
-                        	Winston Foo
-                        </p>
-                        <p>
-                        	www.mobyshop.com.sg
-                        </p>
-                    </div>
-                </div>
             </div>
         </div>
     </div><!-- featured -->
