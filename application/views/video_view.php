dcb9169ebb4c25c0d4685b93411c6576f1513ce3
raviribadiya18 <raviribadiya014@gmail.com>
2020-02-17 10:54:49 +0530

    video page add

--- application/views/video_view.php
+++ application/views/video_view.php
@@ -1,51 +1,51 @@
-	<div class="progress-bar bg-danger" role="progressbar" style="font-size: 40px; width: 100%;  background: rgba(219, 13, 13, 0.2) !important;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-		<span style="color: rgba(219, 13, 13, 1); height: 60px; margin: 20px 0 0 0;">
-		  VIDEO
-		</span>
-	</div>
-	<!-- page-title -->
-	<div class="practice-single">
-		<div class="container">
-			<div class="row">
-				<div class="practice-area practice-area-style1">
-        <div class="container">
-            <div class="title-section text-center">
-                <h3 class="flat-title">Video Gallery</h3>
-            </div>
-            <div class="practice-area-wrap">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="image-box-law">
-                            <div class="featured-post">
-                                <div class="entry-image">
-                                    <iframe width="370" height="216" src="https://www.youtube.com/embed/Xs4zZouqesA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="image-box-law">
-                            <div class="featured-post">
-                                <div class="entry-image">
-                                    <iframe width="370" height="216" src="https://www.youtube.com/embed/Xs4zZouqesA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="image-box-law">
-                            <div class="featured-post">
-                                <div class="entry-image">
-                                    <<iframe width="370" height="216" src="https://www.youtube.com/embed/Xs4zZouqesA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="progress-bar bg-danger" role="progressbar" style="font-size: 40px; width: 100%;  background: rgba(219, 13, 13, 0.2) !important;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+	<span style="color: rgba(219, 13, 13, 1); height: 60px; margin: 20px 0 0 0;">
+	  VIDEO
+	</span>
+</div>
+<!-- page-title -->
+<div class="practice-single">
+	<div class="container">
+		<div class="row">
+			<div class="practice-area practice-area-style1">
+		        <div class="container">
+		            <div class="title-section text-center">
+		                <h3 class="flat-title">Video Gallery</h3>
+		            </div>
+		            <div class="practice-area-wrap">
+		                <div class="row">
+		                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+		                        <div class="image-box-law">
+		                            <div class="featured-post">
+		                                <div class="entry-image">
+		                                    <iframe width="370" height="216" src="https://www.youtube.com/embed/Xs4zZouqesA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		                                </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+		                        <div class="image-box-law">
+		                            <div class="featured-post">
+		                                <div class="entry-image">
+		                                    <iframe width="370" height="216" src="https://www.youtube.com/embed/Xs4zZouqesA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		                                </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+		                        <div class="image-box-law">
+		                            <div class="featured-post">
+		                                <div class="entry-image">
+		                                    <iframe width="370" height="216" src="https://www.youtube.com/embed/Xs4zZouqesA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		                                </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
 			</div>
 		</div>
-	</div><!-- practice-single -->
-	
+	</div>
+</div><!-- practice-single -->
+
